3ec394237a47ff76c86f889f2033207b65f864fc
rrogntudju <you@example.com>
2022-11-06 18:17:20 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use arduino_hal::hal::port::PB5;
 use arduino_hal::port::{mode::Output, Pin};
 use panic_halt as _;
 
-fn blink_code(d13: &mut Pin<Output, PB5>, n: usize) {
+fn clignote_code(led: &mut Pin<Output, PB5>, n: usize) {
     for _ in 0..n {
-        d13.toggle();
+        led.toggle();
         arduino_hal::delay_ms(175);
-        d13.toggle();
+        led.toggle();
         arduino_hal::delay_ms(175);
     }
     arduino_hal::delay_ms(1000);
@@ -20,19 +20,9 @@ fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
-
     let mut led = pins.d13.into_output();
 
     loop {
-        blink_code(&mut led, 3);
+        clignote_code(&mut led, 3);
     }
 }
