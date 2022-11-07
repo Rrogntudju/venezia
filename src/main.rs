66ac41a53f39131b2c93de1a1b20d622bbd8b6cf
rrogntudju <you@example.com>
2022-11-07 10:38:34 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 #![no_std]
 #![no_main]
 
+use core::ops::Range;
 use arduino_hal::hal::port::PB5;
 use arduino_hal::port::{mode::Output, Pin};
 use panic_halt as _;
 
-fn clignote_code(led: &mut Pin<Output, PB5>, n: usize) {
-    for _ in 0..n {
+fn clignote_code(led: &mut Pin<Output, PB5>, r: Range<u16>) {
+    for _ in r {
         led.toggle();
         arduino_hal::delay_ms(175);
         led.toggle();
@@ -19,13 +20,14 @@ fn clignote_code(led: &mut Pin<Output, PB5>, n: usize) {
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
-    let mut adc = arduino_hal::Adc::new(dp.ADC, Default::default());
-    let mut led = pins.d13.into_output();
-    let galvanomètre = pins.a0.into_analog_input(&mut adc);
+
     let crydom = pins.d12.into_output_high();   // Crydom à ON
+    let mut adc = arduino_hal::Adc::new(dp.ADC, Default::default());
+    let voltmètre = pins.a0.into_analog_input(&mut adc);
+    let mut led = pins.d13.into_output();
 
     loop {
-        let voltage = galvanomètre.analog_read(&mut adc);
-        clignote_code(&mut led, 3);
+        let voltage = voltmètre.analog_read(&mut adc);
+        clignote_code(&mut led, 0..1);
     }
 }
