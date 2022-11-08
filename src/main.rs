e9cb78774f63c4730d4e9bc52646edc24bca6390
rrogntudju <you@example.com>
2022-11-08 18:57:33 -0500

    pinne de test 10 secs

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,16 @@
 use arduino_hal::prelude::*;
 use panic_halt as _;
 
+const DELAI: u16 = 3600; // Une heure de fonctionnement
+const DELAI_TEST: u16 = 10;
+
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
     let mut crydom = pins.d12.into_output_high(); // Brancher l'alimentation
+    let test = pins.d8.into_output_high();
     let mut adc = arduino_hal::Adc::new(dp.ADC, Default::default());
     let voltmètre = pins.a0.into_analog_input(&mut adc);
     let mut led = pins.d13.into_output();
@@ -18,7 +22,11 @@ fn main() -> ! {
     arduino_hal::delay_ms(250); // Délai pour constante de temps RC
     let seuil = (voltmètre.analog_read(&mut adc) + 30).clamp(0, 1023);
     ufmt::uwriteln!(&mut serial, "Seuil: {}\r", seuil).void_unwrap();
-    let mut délai: u16 = 3600; // Une heure de fonctionnement
+    let mut délai: u16 = if test.is_set_high() {
+        DELAI
+    } else {
+        DELAI_TEST
+    };
 
     loop {
         let lecture = voltmètre.analog_read(&mut adc);
@@ -36,7 +44,11 @@ fn main() -> ! {
             }
         } else {
             led.set_low();
-            délai = 3600;
+            délai = if test.is_set_high() {
+                DELAI
+            } else {
+                DELAI_TEST
+            };
             crydom.set_high();
         }
         arduino_hal::delay_ms(1000);
